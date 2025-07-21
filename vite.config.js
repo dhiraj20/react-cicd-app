2c816857ea49ba0ceebd36f170f4a0de764276b6
dhiraj <dhiraj.majhi20@gmail.com>
2025-07-21 15:08:51 +0530

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/react-cicd-app/'
+   base: '/dhiraj20.github.io/'
 })
